f54a0e94df436398aa2fc01f97598f84a720943c
Shinnosuke Sawada <6warashi9@gmail.com>
2022-02-24 08:30:11 +0900

    Revert "feat: socketの生成されるディレクトリを変更した"
    ssh remote forwarding requires parent directory exist.
    This reverts commit 3dda5e861d2a40a4d8daf11c10a718346d722a32.

--- cmd/misc.go
+++ cmd/misc.go
@@ -11,9 +11,5 @@ func mustGetSocketPath() string {
 	if err != nil {
 		log.Fatalf("os.UserHomeDir: %v", err)
 	}
-	dir := filepath.Join(home, ".muscat")
-	if err := os.MkdirAll(dir, 0700); err != nil {
-		log.Fatalf("os.MkdirAll: %v", err)
-	}
-	return filepath.Join(dir, "socket")
+	return filepath.Join(home, ".muscat", "socket")
 }
