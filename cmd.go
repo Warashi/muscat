ec91f9ad053b5cfcb2499c2ed6ecd097d3467cbe
Shinnosuke Sawada <6warashi9@gmail.com>
2022-09-12 15:28:51 +0900

    fix: darwinだけevent-loopをまわすようにしてinput methodの変更をうけとれるようにした

--- cmd.go
+++ cmd.go
@@ -27,14 +27,19 @@ import (
 	"github.com/Warashi/muscat/cmd"
 )
 
-func main() {
-	switch os.Args[0] {
-	case "xdg-open":
-		os.Args = append([]string{"muscat", "open"}, os.Args[1:]...)
-	case "pbcopy":
-		os.Args = append([]string{"muscat", "copy"}, os.Args[1:]...)
-	case "pbpaste":
-		os.Args = append([]string{"muscat", "paste"}, os.Args[1:]...)
-	}
-	cmd.Execute()
+func _main() chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		defer close(ch)
+		switch os.Args[0] {
+		case "xdg-open":
+			os.Args = append([]string{"muscat", "open"}, os.Args[1:]...)
+		case "pbcopy":
+			os.Args = append([]string{"muscat", "copy"}, os.Args[1:]...)
+		case "pbpaste":
+			os.Args = append([]string{"muscat", "paste"}, os.Args[1:]...)
+		}
+		cmd.Execute()
+	}()
+	return ch
 }
