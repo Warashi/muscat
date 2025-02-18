630b403b43b412b03c576e4b3fbdf10d42a908f6
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-02-18 09:05:42 +0900

    style: nixfmt

--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
-{ pkgs ? (
+{
+  pkgs ? (
     let
       inherit (builtins) fetchTree fromJSON readFile;
       inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
@@ -18,7 +19,6 @@
   useGolangDesign ? false,
   ...
 }:
-
 buildGoApplication {
   pname = "muscat";
   version = "2.3.1";
@@ -26,34 +26,30 @@ buildGoApplication {
   src = ./.;
   modules = ./gomod2nix.toml;
 
-  tags =
-    if useGolangDesign
-    then ["golangdesign"]
-    else [];
+  tags = if useGolangDesign then [ "golangdesign" ] else [ ];
 
   buildInputs =
-    if stdenv.isDarwin
-    then [
-      darwin.apple_sdk.frameworks.Cocoa
-    ]
-    else if useGolangDesign
-    then [
-      xorg.libX11
-    ]
-    else [];
+    if stdenv.isDarwin then
+      [
+        darwin.apple_sdk.frameworks.Cocoa
+      ]
+    else if useGolangDesign then
+      [
+        xorg.libX11
+      ]
+    else
+      [ ];
 
-  nativeBuildInputs =
-    if (stdenv.isLinux && useGolangDesign)
-    then [makeWrapper]
-    else [];
+  nativeBuildInputs = if (stdenv.isLinux && useGolangDesign) then [ makeWrapper ] else [ ];
 
   postFixup =
-    if (stdenv.isLinux && useGolangDesign)
-    then ''
-      wrapProgram $out/bin/muscat \
-        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [xorg.libX11]}
-    ''
-    else "";
+    if (stdenv.isLinux && useGolangDesign) then
+      ''
+        wrapProgram $out/bin/muscat \
+          --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ xorg.libX11 ]}
+      ''
+    else
+      "";
 
   meta = with lib; {
     description = "remote code development utils";
