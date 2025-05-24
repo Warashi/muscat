6ad6910cffba64504c0b1363dfb18ae38a48432a
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-05-24 10:33:54 +0900

    feat: enhance muscat module with Golang design support and additional build configurations
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- default.nix
+++ default.nix
@@ -1,9 +1,42 @@
 {
   pkgs ? import <nixpkgs> { },
+  useGolangDesign ? false,
+  stdenv ? pkgs.stdenv,
+  makeWrapper ? pkgs.makeWrapper,
+  lib ? pkgs.lib,
+  xorg ? pkgs.xorg,
+  darwin ? pkgs.darwin,
 }:
 pkgs.buildGoLatestModule {
   name = "muscat";
   version = "2.3.1";
   src = ./.;
   vendorHash = "sha256-CpfgeQ+HC53uDWBis2muee5rRuKsR7KcV8aQtygAQEA=";
+
+  tags = if useGolangDesign then [ "golangdesign" ] else [ ];
+
+  buildInputs =
+    if stdenv.isDarwin then
+      [ darwin.apple_sdk.frameworks.Cocoa ]
+    else if useGolangDesign then
+      [ xorg.libX11 ]
+    else
+      [ ];
+
+  nativeBuildInputs = if (stdenv.isLinux && useGolangDesign) then [ makeWrapper ] else [ ];
+
+  postFixup =
+    if (stdenv.isLinux && useGolangDesign) then
+      ''
+        wrapProgram $out/bin/muscat \
+          --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ xorg.libX11 ]}
+      ''
+    else
+      "";
+
+  meta = with lib; {
+    description = "remote code development utils";
+    homepage = "https://github.com/Warashi/muscat";
+    license = licenses.mit;
+  };
 }
